c97502c2e2a12e05f47643d538404d6ad1108745
Chris Powell <cpowell@prylis.com>
2014-08-03 20:52:09 -0700

    Added some new actions to the example

--- goap_weapon_example/goap_weapon_example.cpp
+++ goap_weapon_example/goap_weapon_example.cpp
@@ -17,10 +17,15 @@ int main(void) {
     std::cout << "Weapon example running...\n";
     std::vector<Action> actions;
 
-    Action scout("scout", 5);
+    Action scout("scout stealthily", 5);
     scout.setPrecondition(0, false);
     scout.setEffect(0, true);
     actions.push_back(scout);
+
+    Action run("scout while running", 15);
+    run.setPrecondition(0, false);
+    run.setEffect(0, true);
+    actions.push_back(run);
 
     Action approach("approach enemy", 2);
     approach.setPrecondition(0, true);
@@ -29,25 +34,45 @@ int main(void) {
     approach.setEffect(2, true);
     actions.push_back(approach);
 
-    Action load("load weapon", 1);
+    Action load("load gun", 1);
     load.setPrecondition(3, false);
     load.setPrecondition(4, true);
     load.setEffect(3, true);
     actions.push_back(load);
 
-    Action draw("draw weapon", 1);
+    Action draw("draw gun", 1);
     draw.setPrecondition(4, false);
     draw.setEffect(4, true);
     actions.push_back(draw);
 
-    Action attack("attack enemy", 3);
-    attack.setPrecondition(0, true);
-    attack.setPrecondition(1, false);
-    attack.setPrecondition(2, true);
-    attack.setPrecondition(3, true);
-    attack.setPrecondition(4, true);
-    attack.setEffect(1, true);
-    actions.push_back(attack);
+    Action draw_knife("draw knife", 1);
+    draw_knife.setPrecondition(4, false);
+    draw_knife.setEffect(4, true);
+    actions.push_back(draw_knife);
+
+    Action shoot("shoot enemy", 3);
+    shoot.setPrecondition(0, true);
+    shoot.setPrecondition(1, false);
+    shoot.setPrecondition(2, true);
+    shoot.setPrecondition(3, true);
+    shoot.setPrecondition(4, true);
+    shoot.setEffect(1, true);
+    actions.push_back(shoot);
+
+    Action knife("knife enemy", 3);
+    knife.setPrecondition(0, true);
+    knife.setPrecondition(1, false);
+    knife.setPrecondition(2, true);
+    knife.setPrecondition(4, true);
+    knife.setEffect(1, true);
+    actions.push_back(knife);
+
+    Action destruct("self destruct", 30);
+    destruct.setPrecondition(0, true);
+    destruct.setPrecondition(1, false);
+    destruct.setPrecondition(2, true);
+    destruct.setEffect(1, true);
+    actions.push_back(destruct);
 
     WorldState goalState;
 
