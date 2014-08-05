fa68feef4c9c5156f0ed423ddde319bfc50ca8f3
Chris Powell <cpowell@prylis.com>
2014-08-04 19:28:06 -0700

    Comment out a lot of the output; expand the example.

--- goap/AStar.cpp
+++ goap/AStar.cpp
@@ -70,8 +70,8 @@ void AStar::plan(std::vector<Action>& actions) {
     int iters = 0;
     do {
         ++iters;
-        std::cout << "\n-----------------------\n";
-        std::cout << "Iteration " << iters << std::endl;
+//         std::cout << "\n-----------------------\n";
+//         std::cout << "Iteration " << iters << std::endl;
 
         // If there's nothing left to evaluate, then we have no possible path left
         if (open_.size() == 0) {
@@ -87,7 +87,7 @@ void AStar::plan(std::vector<Action>& actions) {
 //         std::cout << "Closed list\n";
 //         printClosedList();
 
-        std::cout << "\nCurrent is " << current << std::endl;
+        /*        std::cout << "\nCurrent is " << current << std::endl;*/
 
         // Is our current state the goal state? If so, we've found a path, yay.
         if (current.ws_.meetsGoal(goal_)) {
@@ -102,13 +102,13 @@ void AStar::plan(std::vector<Action>& actions) {
         for (auto& action : actions) {
             // for each node REACHABLE from "me":
             if (action.eligibleFor(current.ws_)) {
-                std::cout << "Hmm, " << action.name() << " could work...";
+                //std::cout << "Hmm, " << action.name() << " could work...";
                 WorldState possibility = action.actOn(current.ws_);
-                std::cout << "resulting in " << possibility << std::endl;
+                //std::cout << "resulting in " << possibility << std::endl;
 
                 //   if closed, next
                 if (memberOfClosed(possibility)) {
-                    std::cout << "...but that one's closed out.\n";
+                    //std::cout << "...but that one's closed out.\n";
                     continue;
                 }
 
@@ -125,7 +125,7 @@ void AStar::plan(std::vector<Action>& actions) {
                 } else {
                     //     if my path to it is better (G),
                     if ((current.g_ + action.cost()) < needle->g_) {
-                        std::cout << "My path is better\n";
+                        //std::cout << "My path is better\n";
                         needle->parent_id_ = current.id_;                     //       make me its parent
                         needle->g_ = current.g_ + action.cost();              //       recalc F,G for it
                         needle->h_ = calculateHeuristic(possibility, goal_);
@@ -137,37 +137,3 @@ void AStar::plan(std::vector<Action>& actions) {
     } while (true);
 
 }
-
-
-// for each node REACHABLE from "me":
-//   if closed, next
-//   if not on open list,
-//     add to open list
-//     make me its parent
-//     record f,g,h for it
-//   else (i.e. already on open list)
-//     if my path to it is better (G),
-//       make me its parent
-//       recalc F,G for it
-//       resort open list to account for new F
-
-
-//         c) For each of the 8 squares adjacent to this current square …
-//
-//             If it is not walkable or if it is on the closed list, ignore it.Otherwise do the following.
-//
-//             If it isn’t on the open list, add it to the open list.Make the current square the parent of this square.Record the F, G, and H costs of the square.
-//
-//             If it is on the open list already, check to see if this path to that square is better, using G cost as the measure.
-//A lower G cost means that this is a better path.If so, change the parent of the square to the current square,
-//and recalculate the G and F scores of the square.If you are keeping your open list sorted by F score, you may
-//need to resort the list to account for the change.
-//
-//             d) Stop when you :
-//
-//         Add the target square to the closed list, in which case the path has been found(see note below), or
-//             Fail to find the target square, and the open list is empty.In this case, there is no path.
-
-
-// 3) Save the path. Working backwards from the target square, go from each square to its parent square
-// until you reach the starting square. That is your path.
--- goap_weapon_example/goap_weapon_example.cpp
+++ goap_weapon_example/goap_weapon_example.cpp
@@ -17,58 +17,76 @@ int main(void) {
     std::cout << "Weapon example running...\n";
     std::vector<Action> actions;
 
-    // Action strings
-    const std::string scout_stealthily("scoutStealthily");
-    const std::string scout_running("scoutRunning");
-    const std::string approach_enemy("approachEnemy");
-    const std::string load_gun("loadGun");
-    const std::string draw_gun("drawGun");
-    const std::string draw_knife("drawKnife");
-    const std::string shoot_enemy("shootEnemy");
-    const std::string knife_enemy("knifeEnemy");
-    const std::string self_destruct("selfDestruct")
-    ;
     // State strings
-    const std::string enemy_sighted("enemySighted");
-    const std::string enemy_dead("enemyDead");
-    const std::string enemy_in_range("enemyInWeaponRange");
-    const std::string gun_drawn("gunDrawn");
-    const std::string gun_loaded("gunLoaded");
+    const std::string enemy_sighted("isEnemySighted");
+    const std::string enemy_dead("isEnemyDead");
+    const std::string enemy_in_range("isEnemyInGunRange");
+    const std::string enemy_in_close_range("isEnemyInKnifeRange");
+    const std::string gun_drawn("isGunDrawn");
+    const std::string gun_loaded("isGunLoaded");
+    const std::string knife_drawn("isKnifeDrawn");
+    const std::string weapon_in_hand("isWeaponInHand");
+    const std::string me_dead("isMeDead");
 
-    Action scout(scout_stealthily, 5);
+    Action scout("scoutStealthily", 5);
     scout.setPrecondition(enemy_sighted, false);
     scout.setEffect(enemy_sighted, true);
     actions.push_back(scout);
 
-    Action run(scout_running, 15);
+    Action run("scoutRunning", 15);
     run.setPrecondition(enemy_sighted, false);
     run.setEffect(enemy_sighted, true);
     actions.push_back(run);
 
-    Action approach(approach_enemy, 2);
+    Action approach("closeToGunRange", 2);
     approach.setPrecondition(enemy_sighted, true);
     approach.setPrecondition(enemy_dead, false);
     approach.setPrecondition(enemy_in_range, false);
     approach.setEffect(enemy_in_range, true);
     actions.push_back(approach);
 
-    Action load(load_gun, 1);
+    Action approachClose("closeToKnifeRange", 2);
+    approachClose.setPrecondition(enemy_sighted, true);
+    approachClose.setPrecondition(enemy_dead, false);
+    approachClose.setPrecondition(enemy_in_close_range, false);
+    approachClose.setEffect(enemy_in_close_range, true);
+    actions.push_back(approachClose);
+
+    Action load("loadGun", 2);
     load.setPrecondition(gun_loaded, false);
     load.setPrecondition(gun_drawn, true);
     load.setEffect(gun_loaded, true);
     actions.push_back(load);
 
-    Action draw(draw_gun, 1);
+    Action draw("drawGun", 1);
+    draw.setPrecondition(weapon_in_hand, false);
     draw.setPrecondition(gun_drawn, false);
     draw.setEffect(gun_drawn, true);
+    draw.setEffect(weapon_in_hand, true);
     actions.push_back(draw);
 
-    Action drawKnife(draw_knife, 1);
+    Action holster("holsterGun", 1);
+    holster.setPrecondition(weapon_in_hand, true);
+    holster.setPrecondition(gun_drawn, true);
+    holster.setEffect(gun_drawn, false);
+    holster.setEffect(weapon_in_hand, false);
+    actions.push_back(holster);
+
+    Action drawKnife("drawKnife", 1);
+    drawKnife.setPrecondition(weapon_in_hand, false);
     drawKnife.setPrecondition(gun_drawn, false);
     drawKnife.setEffect(gun_drawn, true);
+    drawKnife.setEffect(weapon_in_hand, true);
     actions.push_back(drawKnife);
 
-    Action shoot(shoot_enemy, 3);
+    Action sheath("sheathKnife", 1);
+    sheath.setPrecondition(weapon_in_hand, true);
+    sheath.setPrecondition(knife_drawn, true);
+    sheath.setEffect(knife_drawn, false);
+    sheath.setEffect(weapon_in_hand, false);
+    actions.push_back(sheath);
+
+    Action shoot("shootEnemy", 3);
     shoot.setPrecondition(enemy_sighted, true);
     shoot.setPrecondition(enemy_dead, false);
     shoot.setPrecondition(gun_drawn, true);
@@ -77,35 +95,46 @@ int main(void) {
     shoot.setEffect(enemy_dead, true);
     actions.push_back(shoot);
 
-    Action knife(knife_enemy, 3);
+    Action knife("knifeEnemy", 3);
     knife.setPrecondition(enemy_sighted, true);
     knife.setPrecondition(enemy_dead, false);
-    knife.setPrecondition(gun_drawn, true);
-    knife.setPrecondition(gun_loaded, true);
-    knife.setPrecondition(enemy_in_range, true);
+    knife.setPrecondition(knife_drawn, true);
+    knife.setPrecondition(enemy_in_close_range, true);
     knife.setEffect(enemy_dead, true);
     actions.push_back(knife);
 
-    Action destruct(self_destruct, 30);
+    Action destruct("selfDestruct", 30);
     destruct.setPrecondition(enemy_sighted, true);
     destruct.setPrecondition(enemy_dead, false);
     destruct.setPrecondition(enemy_in_range, true);
     destruct.setEffect(enemy_dead, true);
+    destruct.setEffect(me_dead, true);
     actions.push_back(destruct);
 
-    WorldState goalState;
-    goalState.setVariable(enemy_dead, true);
+    WorldState goalStateDraw;
+    goalStateDraw.setVariable(enemy_dead, true);
+    goalStateDraw.setVariable(me_dead, true);
+    goalStateDraw.priority_ = 50;
+
+    WorldState goalStateWin;
+    goalStateWin.setVariable(enemy_dead, true);
+    goalStateWin.setVariable(me_dead, false);
+    goalStateWin.priority_ = 100;
 
     WorldState initialState;
     initialState.setVariable(enemy_dead, false);
     initialState.setVariable(enemy_sighted, false);
     initialState.setVariable(enemy_in_range, false);
+    initialState.setVariable(enemy_in_close_range, false);
     initialState.setVariable(gun_loaded, false);
     initialState.setVariable(gun_drawn, false);
+    initialState.setVariable(knife_drawn, true);
+    initialState.setVariable(weapon_in_hand, true);
+    initialState.setVariable(me_dead, false);
 
     AStar as;
     as.setStart(initialState);
-    as.setGoal(goalState);
+    as.setGoal(goalStateWin);
     try {
         as.plan(actions);
     }
@@ -113,6 +142,13 @@ int main(void) {
         std::cout << "Sorry, could not find a path!\n";
     }
 
+    as.setGoal(goalStateDraw);
+    try {
+        as.plan(actions);
+    }
+    catch (const std::exception& ex) {
+        std::cout << "Sorry, could not find a path!\n";
+    }
 
 
 
