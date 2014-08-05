378bd53723791ffd39eb40b0978358a8230b5e0e
Chris Powell <cpowell@prylis.com>
2014-08-04 18:53:31 -0700

    The example works.

--- goap_weapon_example/goap_weapon_example.cpp
+++ goap_weapon_example/goap_weapon_example.cpp
@@ -18,72 +18,72 @@ int main(void) {
     std::vector<Action> actions;
 
     Action scout("scout stealthily", 5);
-    scout.setPrecondition(0, false);
-    scout.setEffect(0, true);
+    scout.setPrecondition("enemySighted", false);
+    scout.setEffect("enemySighted", true);
     actions.push_back(scout);
 
     Action run("scout while running", 15);
-    run.setPrecondition(0, false);
-    run.setEffect(0, true);
+    run.setPrecondition("enemySighted", false);
+    run.setEffect("enemySighted", true);
     actions.push_back(run);
 
     Action approach("approach enemy", 2);
-    approach.setPrecondition(0, true);
-    approach.setPrecondition(1, false);
-    approach.setPrecondition(2, false);
-    approach.setEffect(2, true);
+    approach.setPrecondition("enemySighted", true);
+    approach.setPrecondition("enemyDead", false);
+    approach.setPrecondition("enemyInWeaponRange", false);
+    approach.setEffect("enemyInWeaponRange", true);
     actions.push_back(approach);
 
     Action load("load gun", 1);
-    load.setPrecondition(3, false);
-    load.setPrecondition(4, true);
-    load.setEffect(3, true);
+    load.setPrecondition("gunLoaded", false);
+    load.setPrecondition("gunDrawn", true);
+    load.setEffect("gunLoaded", true);
     actions.push_back(load);
 
     Action draw("draw gun", 1);
-    draw.setPrecondition(4, false);
-    draw.setEffect(4, true);
+    draw.setPrecondition("gunDrawn", false);
+    draw.setEffect("gunDrawn", true);
     actions.push_back(draw);
 
     Action draw_knife("draw knife", 1);
-    draw_knife.setPrecondition(4, false);
-    draw_knife.setEffect(4, true);
+    draw_knife.setPrecondition("gunDrawn", false);
+    draw_knife.setEffect("gunDrawn", true);
     actions.push_back(draw_knife);
 
     Action shoot("shoot enemy", 3);
-    shoot.setPrecondition(0, true);
-    shoot.setPrecondition(1, false);
-    shoot.setPrecondition(2, true);
-    shoot.setPrecondition(3, true);
-    shoot.setPrecondition(4, true);
-    shoot.setEffect(1, true);
+    shoot.setPrecondition("enemySighted", true);
+    shoot.setPrecondition("enemyDead", false);
+    shoot.setPrecondition("gunDrawn", true);
+    shoot.setPrecondition("gunLoaded", true);
+    shoot.setPrecondition("enemyInWeaponRange", true);
+    shoot.setEffect("enemyDead", true);
     actions.push_back(shoot);
 
     Action knife("knife enemy", 3);
-    knife.setPrecondition(0, true);
-    knife.setPrecondition(1, false);
-    knife.setPrecondition(2, true);
-    knife.setPrecondition(4, true);
-    knife.setEffect(1, true);
+    knife.setPrecondition("enemySighted", true);
+    knife.setPrecondition("enemyDead", false);
+    knife.setPrecondition("gunDrawn", true);
+    knife.setPrecondition("gunLoaded", true);
+    knife.setPrecondition("enemyInWeaponRange", true);
+    knife.setEffect("enemyDead", true);
     actions.push_back(knife);
 
     Action destruct("self destruct", 30);
-    destruct.setPrecondition(0, true);
-    destruct.setPrecondition(1, false);
-    destruct.setPrecondition(2, true);
-    destruct.setEffect(1, true);
+    destruct.setPrecondition("enemySighted", true);
+    destruct.setPrecondition("enemyDead", false);
+    destruct.setPrecondition("enemyInWeaponRange", true);
+    destruct.setEffect("enemyDead", true);
     actions.push_back(destruct);
 
     WorldState goalState;
-
-    for (int i = 0; i < 5; ++i) {
-        goalState.var_matters_[i] = false;
-    }
-
-    goalState.state_vars_[1] = true;
-    goalState.var_matters_[1] = true;
+    goalState.setVariable("enemyDead", true);
 
     WorldState initialState;
+    initialState.setVariable("enemyDead", false);
+    initialState.setVariable("enemySighted", false);
+    initialState.setVariable("enemyInWeaponRange", false);
+    initialState.setVariable("gunLoaded", false);
+    initialState.setVariable("gunDrawn", false);
 
     AStar as;
     as.setStart(initialState);
